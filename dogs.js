d85f6d3cfec1a8dda59d73d65066b58854ab2289
Rachel Fullerton <rachfull00@gmail.com>
2022-03-21 14:04:32 -0600

    fixed mp4 exception in javascript

--- dogs.js
+++ dogs.js
@@ -10,7 +10,15 @@ document.getElementById("dogPicSubmit").addEventListener("click", function(event
 
       let results = "";
       results += "<h2>Please enjoy this picture of a dog :)</h2>";
-      results += '<img src="' + json.url + '"/>';
+
+      let imgUrl = json.url;
+      let imgEnd = imgUrl.substring(imgUrl.length - 4, imgUrl.length);
+      if ( imgEnd == ".mp4") {
+        // imgUrl = url("photos/doggie.jpg");
+        results += "<img src=" + "photos/doggie.jpg" + ">"; 
+      } else {
+        results += '<img src="' + json.url + '"/>';
+      }
 
       document.getElementById("dogPicResults").innerHTML = results;
     })
